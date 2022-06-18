9d164efd279523b5a4804d09ce7018ec73d64c67
Eamon McHugh <eamonmchugh2@gmail.com>
2022-06-18 12:39:16 -0400

    updated development config file. devServer write to disk property needed to be wrapped. Was not outputting public folder

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExstractPlugin = require('mini-css-extract-plugin');
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
+
+const dirApp = path.join(__dirname, 'app');
+const dirShared = path.join(__dirname, 'shared');
+const dirStyles = path.join(__dirname, 'styles');
+const dirNode = 'node_modules';
+
+module.exports = {
+	entry: [path.join(dirApp, 'index.js'), path.join(dirStyles, 'index.scss')],
+
+	resolve: {
+		modules: [dirApp, dirShared, dirStyles, dirNode],
+	},
+};
